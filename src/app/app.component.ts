2996eab1d53a427d372e2de50c64de1281a1fa98
IsaiVelasco <velascohernandes.2017@gmail.com>
2022-12-26 23:47:57 -0600

    Add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,13 @@ export class AppComponent {
   art = {
     codigo:0,
     descripcion:"",
-    precio:0
+    precio:0,
+    imagen:""
   }
   articulos=[
-    {codigo:1, descripcion:"Papas", precio: 15.00},
-    {codigo:2, descripcion:"Donas", precio: 15.00},
-    {codigo:3, descripcion:"Pan cortado", precio: 15.00},
-    {codigo:4, descripcion:"Aceite", precio: 15.00},
-    {codigo:5, descripcion:"Cafe", precio: 15.00},
+    {codigo:1, descripcion:"Papas", precio: 15.00, imagen:"https://itengoo.com/wp-content/uploads/2021/09/81NxIieAiWS._SL1500_.jpg"},
+    {codigo:2, descripcion:"Donas", precio: 15.00,imagen:"https://res.cloudinary.com/walmart-labs/image/upload/d_default.jpg/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750100011250L.jpg"},
+    {codigo:3, descripcion:"Pan cortado", precio: 15.00,imagen:"https://frutilandiago.com/img/p/7/1/1/711-large_default.jpg"},
   ]
   hayRegistros(){
     return this.articulos.length>0
@@ -45,22 +44,26 @@ export class AppComponent {
     this.articulos.push({
       codigo:this.art.codigo,
       descripcion:this.art.descripcion,
-      precio:this.art.precio
+      precio:this.art.precio,
+      imagen:this.art.imagen
     })
     this.art.codigo=0;
     this.art.descripcion="";
     this.art.precio=0;
+    this.art.imagen="";
   }
-  seleccionar(art:{codigo:number;descripcion:string;precio:number}){
+  seleccionar(art:{codigo:number;descripcion:string;precio:number; imagen:string}){
     this.art.codigo=art.codigo;
     this.art.descripcion=art.descripcion;
     this.art.precio=art.precio;
+    this.art.imagen = art.imagen;
   } 
   modificar(){
     this.articulos.forEach(a => {
       if(a.codigo == this.art.codigo){
         a.descripcion = this.art.descripcion;
         a.precio = this.art.precio;
+        a.imagen = this.art.imagen;
         return;
       }
     });
